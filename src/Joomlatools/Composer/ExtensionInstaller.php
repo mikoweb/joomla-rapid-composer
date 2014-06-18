be0991e396460f09441d645690a1e2606e966665
Rafał Mikołajun <rafal@mikoweb.pl>
2014-06-18 20:13:03 +0200

    Update ExtensionInstaller.php

--- src/Joomlatools/Composer/ExtensionInstaller.php
+++ src/Joomlatools/Composer/ExtensionInstaller.php
@@ -168,19 +168,21 @@ class ExtensionInstaller extends LibraryInstaller
             define('DS', DIRECTORY_SEPARATOR);
 
             define('JPATH_BASE', realpath('.') . (isset($config['basedir']) ? '/' . $config['basedir'] : ''));
+            
+            if (isset($config['component-symlink']) && is_string($config['component-symlink'])) {
+        		// Define component path.
+        		define('JPATH_COMPONENT', JPATH_BASE . '/' . $config['component-symlink']);
+        		define('JPATH_COMPONENT_SITE', JPATH_SITE . '/' . $config['component-symlink']);
+        		define('JPATH_COMPONENT_ADMINISTRATOR', JPATH_ADMINISTRATOR . '/' . $config['component-symlink']);
+        		var_dump($config);
+            }
+            
             require_once JPATH_BASE . '/includes/defines.php';
 
             require_once JPATH_BASE . '/includes/framework.php';
             require_once JPATH_LIBRARIES . '/import.php';
 
             require_once JPATH_LIBRARIES . '/cms.php';
-            
-            if (isset($config['component-symlink']) && is_string($config['component-symlink'])) {
-        		// Define component path.
-        		define('JPATH_COMPONENT', JPATH_BASE . '/' . $config['component-symlink']);
-        		define('JPATH_COMPONENT_SITE', JPATH_SITE . '/' . $config['component-symlink']);
-        		define('JPATH_COMPONENT_ADMINISTRATOR', JPATH_ADMINISTRATOR . '/' . $config['component-symlink']);                                
-            }
         }
 
         if(!($this->_application instanceof Application))
